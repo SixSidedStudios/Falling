f56b6f9cbd22c472200f769d5c3816a8da25d3cc
unknown <public@LAB11.ICPL.INT>
2015-01-24 14:43:31 -0600

    Initial commit
    -Added intro

---
+++ falling.py
@@ -0,0 +1,50 @@
+import time
+import os
+
+# Intro
+print '"Look", you say. "I didn\'t want to do this."'
+time.sleep(3)
+print '"So why are you taking me up anyway?"'
+time.sleep(2)
+os.system('cls')
+
+# Use pausing to imitate animation
+print '----FALLING----'
+time.sleep(1)
+os.system('cls')
+print '----       ----'
+print '    FALLING    '
+time.sleep(0.5)
+os.system('cls')
+print '----       ----'
+print '               '
+print '    FALLING    '
+time.sleep(0.5)
+os.system('cls')
+print '----       ----'
+print '               '
+print '               '
+print '    FALLING    '
+time.sleep(0.5)
+os.system('cls')
+print '----       ----'
+print '               '
+print '               '
+print '               '
+print '    FALLING    '
+time.sleep(0.5)
+os.system('cls')
+print '----       ----'
+print '               '
+print '               '
+print '               '
+print '               '
+print '    FALLING    '
+
+time.sleep(1)
+print ''
+print 'Press ENTER to begin'
+x = raw_input('')
+
+
+
